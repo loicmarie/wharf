37586483c07cfdc5fcbbc84ff6094cb6cfd5ebe4
Loic MARIE <loic.marie@ysance.com>
2016-08-13 23:51:36 +0000

    Init git flow. Begin dockerfiles page edition

--- gulpfile.js
+++ gulpfile.js
@@ -216,7 +216,7 @@ gulp.task('clean', function() {
 // Watch files for changes & reload
 gulp.task('serve', ['styles'], function() {
   browserSync({
-    port: 5000,
+    port: 8080,
     notify: false,
     logPrefix: 'PSK',
     snippetOptions: {
